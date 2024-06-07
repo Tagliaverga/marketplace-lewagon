532265b378b24ac19e8d5713a3ae365f45e19b67
Ricardo Tagliaverga tagliaverga@icloud.com <tagliaverga@icloud.com>
2024-06-07 16:03:08 -0300

    hisórico de compras

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @sales = Sale.all
-    @sale = Sale.where(params[:user_id])
   end
 
   def historic
